95b70841dd448edc33343e82b7e099fac3d04cf1
Ezhavarasan-78 <ezhavarasan2003b@gmail.com>
2026-03-17 18:11:40 +0530

    C# hands on files were added

---
+++ week-5/day22/Bank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day22
+{
+    public class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException(string message) : base(message)
+        {
+        }
+    }
+    class BankAccount
+    {
+        private double balance;
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+        public void Withdraw(double w)
+        {
+            if(w>balance)
+            {
+                throw new InsufficientBalanceException("Insufficient balance");
+            }
+                balance = balance - w;
+            Console.WriteLine("withdraw successfully available balance : " + balance);
+        }
+    }
+    internal class Bank
+    {
+        static void Main(string[] args)
+        {
+            double balance = 2000;
+            double withdraw = 5000;
+            BankAccount acc = new BankAccount(balance);
+            try
+            {
+                acc.Withdraw(withdraw);
+            }
+            catch(InsufficientBalanceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Transaction Complete");
+            }
+        }
+    }
+}
---
+++ week-5/day22/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace day22
+{
+    class Calculator
+    {
+        public void div(int num,int den)
+        {
+            try
+            {
+                int res = num / den;
+                Console.WriteLine("result " + res);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("error cannot divide by zero");
+            }
+            finally
+            {
+                Console.WriteLine("completed");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 20;
+            int den = 0;
+            Calculator cal = new Calculator();
+            cal.div(num, den);
+            Console.WriteLine("program runs");
+        }
+    }
+}
