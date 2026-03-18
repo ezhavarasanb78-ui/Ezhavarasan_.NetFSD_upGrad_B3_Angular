8ffc69ea2f777f4d82ecebd2bb95d4836f5f1f67
Ezhavarasan-78 <ezhavarasan2003b@gmail.com>
2026-03-18 19:03:22 +0530

    day23 C# hands on files were added

---
+++ week-5/day23/Linq.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day23
+{
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Mrp { get; set; }
+    }
+    internal class Linq
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                  new Product { Code = 1, Name = "Soap", Category = "FMCG", Mrp = 25 },
+                  new Product { Code = 2, Name = "Rice", Category = "Grain", Mrp = 50 },
+                  new Product { Code = 3, Name = "Oil", Category = "FMCG", Mrp = 120 },
+                  new Product { Code = 4, Name = "Wheat", Category = "Grain", Mrp = 40 },
+                  new Product { Code = 5, Name = "Shampoo", Category = "FMCG", Mrp = 30 }
+             };
+
+            var fmcg = products.Where(p => p.Category == "FMCG");
+            Console.WriteLine(fmcg);
+
+            foreach(var i in fmcg)
+            {
+                Console.WriteLine($"{i.Name}");
+            }
+            var grain = products.Where(c => c.Category == "Grain");
+            foreach (var i in grain)
+            {
+                Console.WriteLine($"{i.Name}");
+            }
+            var s = products.OrderBy(p => p.Name);
+            foreach (var i in s)
+            {
+                Console.WriteLine($"{i.Name}");
+            }
+            var c = products.OrderBy(p => p.Code);
+            foreach (var i in c)
+            {
+                Console.WriteLine($"{i.Code}");
+            }
+            var d = products.OrderBy(p => p.Mrp);
+            var e = products.OrderByDescending(p => p.Mrp);
+            var g = products.GroupBy(p => p.Category);
+            foreach(var i in g)
+            {
+                Console.WriteLine($"{i.Key}");
+                foreach(var j in i)
+                {
+                    Console.WriteLine(j.Name);
+                }
+            }
+            var h = products.GroupBy(p => p.Mrp);
+            var maxFmcg = products
+.           Where(p => p.Category == "FMCG")
+.           OrderByDescending(p => p.Mrp)
+.           FirstOrDefault();
+            var tot = products.Count();
+            var fmcgcount = products.Count(p => p.Category == "FMCG");
+            var mx = products.Max(p => p.Mrp);
+            var mi = products.Min(p => p.Mrp);
+            var k = products.All(p => p.Mrp>30);
+            var l = products.All(p => p.Mrp < 30);
+        }
+    }
+}
---
+++ week-5/day23/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace day23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> task = new List<string>();
+            while(true)
+            {
+                Console.WriteLine("1.Add task");
+                Console.WriteLine("2.view task");
+                Console.WriteLine("3.remove task");
+                Console.WriteLine("4.exits");
+                Console.WriteLine("Enter the option");
+                string str = Console.ReadLine();
+                switch(str)
+                {
+                    case "1":
+                        addtask(task);
+                        break;
+                    case "2":
+                        viewtask(task);
+                        break;
+                    case "3":
+                        removetask(task);
+                        break;
+                    case "4":
+                        Console.WriteLine("exiting");
+                        break;
+                }
+            }
+        }
+        static void addtask(List<string> task)
+        {
+            Console.WriteLine("enter task");
+            string ta = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(ta))
+            {
+                task.Add(ta);
+                Console.WriteLine("task added successfully");
+            }
+            else
+            {
+                Console.WriteLine("task cannot be empty");
+            }
+        }
+        static void viewtask(List<string> task)
+        {
+            if(task.Count==0)
+            {
+                Console.WriteLine("no tasks available");
+            }
+            else
+            {
+                Console.Write("tasks");
+                for(int i=0;i<task.Count;i++)
+                {
+                    Console.WriteLine($"{i+1}.{task[i]}");
+                }
+            }
+        }
+        static void removetask(List<string> task)
+        {
+            if(task.Count==0)
+            {
+                Console.WriteLine("no task to remove");
+                return;
+            }
+            Console.WriteLine("enter task to remove");
+            string inp = Console.ReadLine();
+            if(int.TryParse(inp,out int tn))
+            {
+                if(tn>=1 && tn<=task.Count)
+                {
+                    string rt = task[tn - 1];
+                    task.RemoveAt(tn - 1);
+                    Console.WriteLine($"removed task {rt}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid task");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter correct task number");
+            }
+
+        }
+    }
+}
