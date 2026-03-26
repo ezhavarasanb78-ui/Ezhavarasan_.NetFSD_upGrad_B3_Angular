16e9950ffdc944705edc767064993f0c69191cd0
Ezhavarasan-78 <ezhavarasan2003b@gmail.com>
2026-03-26 22:26:40 +0530

    day 28 ASP.NET Hands on files were added

---
+++ week-6/day28/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+namespace WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product{ Id=1, Name="Laptop", Category="Electronics", Price=50000 },
+            new Product{ Id=2, Name="Mobile", Category="Electronics", Price=20000 },
+            new Product{ Id=3, Name="Chair", Category="Furniture", Price=3000 }
+        };
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+            return View(product);
+        }
+    }
+}
