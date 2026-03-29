1e3e1ed420c76edc9337b0f4d428c3b1ac7c83a8
Ezhavarasan-78 <ezhavarasan2003b@gmail.com>
2026-03-29 22:41:10 +0530

    day 29 ASP.NET hands on files were added

---
+++ week-6/day29/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDAPP.Models;
+using System.Collections.Generic;
+using System.Linq;
+namespace CRUDAPP.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product>  prod=new List<Product>()
+        {
+           new Product { Id = 1, Name = "Laptop", Price = 50000, Category = "Electronics" },
+           new Product { Id = 2, Name = "Mobile", Price = 20000, Category = "Electronics" }
+        };
+        public IActionResult Index()
+        {
+            return View(prod);
+        }
+        public IActionResult Details(int id)
+        {
+            var product = prod.FirstOrDefault(x => x.Id == id);
+            return View(product);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if(ModelState.IsValid)
+            {
+                prod.Add(p);
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+        public IActionResult Edit(int id)
+        {
+            var product = prod.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product p)
+        {
+            if(ModelState.IsValid)
+            {
+                var product = prod.FirstOrDefault(x => x.Id == p.Id);
+                product.Name = p.Name;
+                product.Price = p.Price;
+                product.Category = p.Category;
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+        public IActionResult Delete(int id)
+        {
+            var product = prod.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult Deleteconfirmed(int id)
+        {
+            var product = prod.FirstOrDefault(x => x.Id == id);
+            prod.Remove(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-6/day29/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAPP.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Product Id is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be 5 to 15 characters")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Price is required")]
+        public string Category { get; set; }
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Category must be 5 to 15 characters")]
+        public decimal Price { get; set; }
+    }
+}
