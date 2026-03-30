173f58a6fd07dd387e3d35c1f90d7caa2d3501b0
Ezhavarasan-78 <ezhavarasan2003b@gmail.com>
2026-03-30 18:03:33 +0530

    day 30 ASP.NET hands on files were added

---
+++ week-7/day30/ContactApp/ContactApp/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using ContactApp.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace ContactApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        private static List<ContactInfo> cont = new List<ContactInfo>()
+        {
+            new ContactInfo { Id = 1, FirstName = "John", LastName = "Doe", CompanyName = "ABC Corp", EmailId = "john@gmail.com", MobileNo = 9876543210, Designation = "Manager" },
+            new ContactInfo { Id = 2, FirstName = "Jane", LastName = "Smith", CompanyName = "XYZ Ltd", EmailId = "jane@gmail.com", MobileNo = 9123456780, Designation = "Developer" }
+        };
+        public IActionResult ShowContact()
+        {
+            return View(cont);
+        }
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo ci)
+        {
+            cont.Add(ci);
+            return RedirectToAction("ShowContact");
+        }
+        public IActionResult GetContactbyId(int id)
+        {
+            var contact = cont.FirstOrDefault(c => c.Id == id);
+            if(contact==null)
+            {
+                ViewBag.Message = "Contact not found";
+                return View();
+            }
+            return View(contact);
+        }
+
+    }
+}
